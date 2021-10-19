eb3af30fa86792dc70fe511c430f17dca7d12c56
Amit Baid <baidstar@hotmail.com>
2021-10-19 13:42:42 -0600

    update app welcome title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">New Project! Ya</div>;
+  return <div className="App">Welcome to Finance Manager</div>;
 }
 
 export default App;
