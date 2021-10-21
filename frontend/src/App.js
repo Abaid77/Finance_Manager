928d8e3cb18c54b999e46a1fdd110e617651b55d
Amit Baid <baidstar@hotmail.com>
2021-10-20 20:15:15 -0600

    start layout for user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to Finance Manager</div>;
+  return (
+    <>
+      <div className="App">Welcome to Finance Manager</div>
+      <div>Login</div>
+      <div>Username:</div>
+      <div>Password:</div>
+    </>
+  );
 }
 
 export default App;
