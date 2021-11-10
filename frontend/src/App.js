55d6e53720de69c9da582bb3dbd142ed6978c0ea
Amit Baid <baidstar@hotmail.com>
2021-11-10 16:38:31 -0700

    add pitches table layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import AppBar from "./components/AppBar";
+import Table from "./components/Table";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
     <>
       <AppBar />
       <div className="App">Welcome to Finance Manager</div>
+      <div>
+        <Grid>
+          <Grid>
+            <Table />
+          </Grid>
+        </Grid>
+      </div>
     </>
   );
 }
